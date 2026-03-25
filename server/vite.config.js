1545df5a6bed28407a11078ced5e881dff202003
BIGGUZMAN <92229099+BIGGUZMAN@users.noreply.github.com>
2026-03-25 16:55:51 +0000

    🔧 CHORE: Vite configuration in progress...

---
+++ server/vite.config.js
@@ -0,0 +1,7 @@
+//importo la funcion de configuracion de  vite 
+import { defineConfig } from 'vite';
+//importo un resolvedor de rutas 
+import { resolver } from "node:path";
+
+//exprotar una instancia de configuracion 
+export default defineConfig({})
