9fb55522a7b0c03a05167e8c68ef6d3b068f6978
BIGGUZMAN <92229099+BIGGUZMAN@users.noreply.github.com>
2026-03-25 16:40:26 +0000

    ♻️ REFACTOR: Add module import alisases add module imports for routing files in app.js and update package.json

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ import logger from 'morgan';
 import { fileURLToPath } from 'node:url';
 
 // 🔥 Rutas
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
-import authorRouter from './routes/author.js';
+import indexRouter from '#routes/index.js';
+import usersRouter from '#routes/users.js';
+import authorRouter from '#routes/author.js';
 
 // 🔥 Recrear __filename y __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
